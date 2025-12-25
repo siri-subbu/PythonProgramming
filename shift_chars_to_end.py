a12e334749f4bf9d8daf5d6940be19ce26f368d4
siri-subbu <ksnssarwani@gmail.com>
2025-12-25 13:05:24 +0530

    Shifting characters from beginning to ending of the string

--- shift_chars_to_end.py
+++ shift_chars_to_end.py
@@ -1,6 +1,14 @@
+'''Program to
+    1.Take a string as input from the user
+    2.Take an integer as shift value from the user
+    3.Transfer x characters where x equals shift value from beginning to the end of the same string '''
+
 st=input("Enter a string :")
 sh=int(input("Enter shift value :"))
+
 nst=""
+
 nst+=st[sh:]
 nst+=st[:sh]
+
 print(nst)
