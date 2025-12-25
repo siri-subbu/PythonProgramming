c3378b19c23708bf81352e7bed477d2b2c87e8ec
siri-subbu <ksnssarwani@gmail.com>
2025-12-25 12:07:52 +0530

    Count number of lower case alphabets in a user defined string without builtin methods

---
+++ count_lower.py
@@ -0,0 +1,12 @@
+'''Program to
+    1.take a string as input from the user
+    2.Count the number of lower case alphabets in entire string and display'''
+
+st=input("Enter a string :")
+c=0
+
+for letter in st:
+    if ord(letter)>=97 and ord(letter)<=122:
+        c+=1
+
+print(f"The count of lower letters is {c}")
