7342b0923f8a8955fa3672155618772121f95cb1
siri-subbu <ksnssarwani@gmail.com>
2025-12-25 10:01:02 +0530

    Removing the duplicate elements from user defined list

--- remove_duplicates.py
+++ remove_duplicates.py
@@ -1,3 +1,11 @@
+'''Program to 
+    1.Define an empty list
+    2.Prompt the user to enter the size of the list
+    3.Prompt the user to enter the integer elements to the defined list
+    4.Display the list
+    5.Define another empty list
+    6.Add the elements from the first list to the new list by removing duplicate elements
+    7.Display the list from which duplicates are removed'''
 n=int(input("Enter the size of the list: "))
 lst=[]
 for i in range(n):
